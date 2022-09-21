70c24e889b8dafc623acabcc81bc1c34d602010d
ariser <n.trshkv@gmail.com>
2022-09-21 17:39:48 +0300

    chore: remove deprecated i18n export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from './components/NavigationTree';
 export * from './components/TreeView';
-// TODO: remove in the next major version
-export {i18n} from './components/i18n';
 export * from './utils';
