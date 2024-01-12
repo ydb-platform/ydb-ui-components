e910cad46f87be92fc60dcba88e7ace47c5da42a
Vladimir Lewandowski <me@vovaspace.com>
2024-01-12 12:39:52 +0100

    feat(TreeView): remove `react-treeview` package to avoid using native `addEventListener`

--- src/components/TreeView/__stories__/TreeView.stories.tsx
+++ src/components/TreeView/__stories__/TreeView.stories.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import {Button} from '@gravity-ui/uikit';
 import {Meta, Story} from '@storybook/react';
+import React, {useState} from 'react';
 import {TreeView, TreeViewProps} from '../TreeView';
 
 export default {
@@ -7,11 +8,60 @@ export default {
     component: TreeView,
 } as Meta<TreeViewProps>;
 
-export const Default: Story<TreeViewProps> = () => (
-    <TreeView name="Root" hasArrow collapsed={false}>
-        <TreeView name="Folder 1" active hasArrow collapsed={false}>
-            <TreeView name="Item 1" onClick={() => console.log(1)} />
+export const Default: Story<TreeViewProps> = () => {
+    const [state, setState] = useState<Record<string, boolean>>({'0': true});
+
+    return (
+        <TreeView
+            name="Root"
+            hasArrow
+            collapsed={!state['0']}
+            actions={[{text: 'Show Directory', action: console.log}]}
+            additionalNodeElements={
+                <Button size="s" onClick={console.log}>
+                    Make
+                </Button>
+            }
+            onClick={() =>
+                setState((current) => ({
+                    ...current,
+                    '0': !current['0'],
+                }))
+            }
+        >
+            <TreeView
+                name="Folder 1"
+                active
+                hasArrow
+                collapsed={!state['1']}
+                onClick={() =>
+                    setState((current) => ({
+                        ...current,
+                        '1': !current['1'],
+                    }))
+                }
+            >
+                <TreeView
+                    name="Item 1"
+                    collapsed={!state['1-1']}
+                    onClick={() =>
+                        setState((current) => ({
+                            ...current,
+                            '1-1': !current['1-1'],
+                        }))
+                    }
+                />
+            </TreeView>
+            <TreeView
+                name="Item 2"
+                collapsed={!state['2']}
+                onClick={() =>
+                    setState((current) => ({
+                        ...current,
+                        '2': !current['2'],
+                    }))
+                }
+            />
         </TreeView>
-        <TreeView name="Item 2" onClick={() => console.log(2)} />
-    </TreeView>
-);
+    );
+};
